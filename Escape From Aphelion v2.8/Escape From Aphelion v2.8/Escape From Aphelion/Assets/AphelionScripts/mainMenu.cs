086de331b906c16f1467866a6f890167246e8443
uthara thelagar <thelagar@usc.edu>
2015-03-27 20:43:25 -0700

    changes

--- Escape From Aphelion v2.8/Escape From Aphelion v2.8/Escape From Aphelion/Assets/AphelionScripts/mainMenu.cs
+++ Escape From Aphelion v2.8/Escape From Aphelion v2.8/Escape From Aphelion/Assets/AphelionScripts/mainMenu.cs
@@ -6,12 +6,12 @@ public class mainMenu : MonoBehaviour {
 	
 	void OnGUI()
 	{
-		if(GUI.Button(new Rect(Screen.width/4.1f, Screen.height/2f, Screen.width/4, Screen.height/10),"New Game"))
+		if(GUI.Button(new Rect(Screen.width/5.1f, Screen.height/2f, Screen.width/4, Screen.height/10),"New Game"))
 		{
 			Application.LoadLevel("SceneWithRotation");
 		}
 		
-		if(GUI.Button(new Rect(Screen.width/4.1f, Screen.height/1.5f, Screen.width/4, Screen.height/10),"Exit Game"))
+		if(GUI.Button(new Rect(Screen.width/5.1f, Screen.height/1.5f, Screen.width/4, Screen.height/10),"Exit Game"))
 		{
 			Application.Quit();
 		}
---
+++ Escape From Aphelion v2.8/Escape From Aphelion v2.8/Escape From Aphelion/Assets/GlowScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class GlowScript : MonoBehaviour {
+	// Use this for initialization
+	public Camera cam;
+	//GameObject camera;
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+		if(cam){
+			Matrix4x4 viewm=cam.cameraToWorldMatrix;
+			Debug.Log(viewm);
+			renderer.sharedMaterial.SetMatrix("ViewIXf",viewm.inverse);
+		}
+	}
+}
